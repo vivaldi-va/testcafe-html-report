2924968cfe5fcfc28b01797b8c7e1cdfffe0de00
Zaccary Price <wildtangomedia@gmail.com>
2021-09-30 22:25:37 +0100

    use node to build templates

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const { copyFile } = require('fs/promises');
+const pug = require('pug');
+const { getInput } = require('@actions/core');
+
+if (!fs.existsSync('build')) {
+  fs.mkdirSync('build');
+}
+
+async function copyAssets() {
+  return copyFile('src/styles.css', 'build/styles.css');
+  return new Promise((resolve, reject) => {
+    fs.copyFile('src/styles.css', 'build/styles.css', (err) => {
+      if (err) {
+        console.error(err);
+        return reject(err);
+      }
+
+      return resolve();
+    });
+  });
+}
+
+async function run() {
+  const writeStream = fs.createWriteStream('build/index.html');
+  const templatePath = 'src/templates/index.pug';
+
+  const jsonPath = getInput('json_report');
+  const jsonReport = JSON.parse(fs.readFileSync(jsonPath));
+
+  const compileFunction = pug.compileFile(templatePath, { pretty: true });
+
+  const compiledTemplate = compileFunction(jsonReport);
+  writeStream.write(compiledTemplate);
+  writeStream.end();
+
+  await copyAssets();
+}
+
+try {
+  run();
+} catch (err) {
+  throw err;
+}
