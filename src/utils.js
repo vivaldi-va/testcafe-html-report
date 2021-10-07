dd3cff2db7896c7aa332c61be5497a57c253bac9
Zaccary Price <wildtangomedia@gmail.com>
2021-10-07 10:47:20 +0100

    prefix screenshot path with ./

--- src/utils.js
+++ src/utils.js
@@ -15,7 +15,8 @@ module.exports.parseData = function parseData(data) {
         // modify screenshot path to point to workspace volume path
         // https://github.community/t/how-can-i-access-the-current-repo-context-and-files-from-a-docker-container-action/17711/2
         screenshotPath: test.screenshotPath && test.screenshotPath
-          .replace(workspace, ''),
+          .replace(workspace, '')
+          .replace(/^\//, './'),
       }))
     }))
   }
